748c65cab7e3bb35bfbf2f61f8ee1e43645933d4
AmitaiB <blickstein@gmail.com>
2015-08-23 23:45:36 -0400

    Initial commit

---
+++ Librarius/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 8/23/15.
+//  Copyright (c) 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Librarius
 A home-library inventory and shelving utility app
+
+
+## UX Case...
+under construction
