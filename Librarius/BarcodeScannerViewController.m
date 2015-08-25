59e47b5447a15d10575b140c6a4cc31f63b235d8
AmitaiB <blickstein@gmail.com>
2015-08-24 22:02:03 -0400

    barcode scanner is up and running

--- Librarius/BarcodeScannerViewController.m
+++ Librarius/BarcodeScannerViewController.m
@@ -7,16 +7,24 @@
 //
 
 #import "BarcodeScannerViewController.h"
+#import <MTBBarcodeScanner.h>
 
 @interface BarcodeScannerViewController ()
+- (IBAction)scanOneButtonTapped:(id)sender;
+- (IBAction)scanContinuouslyButtonTapped:(id)sender;
+- (IBAction)cameraButtonTapped:(id)sender;
 
 @end
 
-@implementation BarcodeScannerViewController
+@implementation BarcodeScannerViewController {
+    MTBBarcodeScanner *scanner;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    scanner = [[MTBBarcodeScanner alloc] initWithPreviewView:self.view];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +32,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)scanOneButtonTapped:(id)sender {
+    [MTBBarcodeScanner requestCameraPermissionWithSuccess:^(BOOL success) {
+        if (success) {
+            [scanner startScanningWithResultBlock:^(NSArray *codes) {
+                AVMetadataMachineReadableCodeObject *code = [codes firstObject];
+                NSLog(@"Found barcode: %@", code.stringValue);
+                
+                [scanner stopScanning];
+            }];
+        } else {
+                //The user denied access to the camera
+            NSLog(@"The user denied access to the camera...?");
+        }
+    }];
+    
+    
+}
+
+- (IBAction)scanContinuouslyButtonTapped:(id)sender {
+    NSMutableArray *uniqueCodes = [NSMutableArray new];
+    [scanner startScanningWithResultBlock:^(NSArray *codes) {
+        for (AVMetadataMachineReadableCodeObject *code in codes) {
+            if ([uniqueCodes indexOfObject:code.stringValue] == NSNotFound) {
+                [uniqueCodes addObject:code.stringValue];
+                NSLog(@"Found unique code: %@", code.stringValue);
+            }
+        }
+    }];
+}
+
+- (IBAction)cameraButtonTapped:(id)sender {
+    [scanner flipCamera];
+}
 @end
