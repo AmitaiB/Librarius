faa5ec5805ff048dc3bf3fc5060ef3e85ca0ee73
AmitaiB <blickstein@gmail.com>
2015-08-25 17:24:22 -0400

    impl retrieveVolumeWithID

--- Librarius/LBRGoogleAPIClient.m
+++ Librarius/LBRGoogleAPIClient.m
@@ -24,13 +24,12 @@
 }
 
 -(void)retrieveVolumeWithID:(NSString*)volumeIDString {
-#warning This is not configured.
-    [self.sessionManager GET:self.gBooksBaseURL parameters:nil success:^nullable void(NSURLSessionDataTask * task, NSDictionary * responseObject)
+    [self.sessionManager GET:self.gBooksBaseURL parameters:nil success:^(NSURLSessionDataTask * task, NSDictionary * responseObject)
  {
          //success → get volume.
-    } failure:^nullable void(NSURLSessionDataTask * task, NSError * error) {
-        NSLog(@"Error in %@: %@", [NSStringFromSelector(_cmd),@"");
-    }
+    } failure:^(NSURLSessionDataTask * task, NSError * error) {
+        NSLog(@"Error in retrieveVolumeWithID: %@", error.localizedDescription);
+    }];
 }
 
 -(void)retrieveVolumesWithQuery:(NSString*)queryString {
