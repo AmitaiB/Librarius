9971fcab52b5487d44cc4d5dba4104ec1bda6cbd
AmitaiB <blickstein@gmail.com>
2015-08-25 21:35:25 -0400

    begins to weave in Core Data: adds Master-Detail from a CoreData template

--- Librarius/AppDelegate.h
+++ Librarius/AppDelegate.h
@@ -8,11 +8,18 @@
 
 #import <UIKit/UIKit.h>
 #import <Google/SignIn.h>
+#import <CoreData/CoreData.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, GIDSignInDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (readonly, nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+@property (readonly, nonatomic, strong) NSManagedObjectModel *managedObjectModel;
+@property (readonly, nonatomic, strong) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+-(void)saveContext;
+-(NSURL *)applicationDocumentsDirectory;
 
 @end
 
