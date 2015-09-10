dc21bd1fd4b7a017a4d07cfd5c69f92a2df9c161
AmitaiB <blickstein@gmail.com>
2015-09-09 20:02:28 -0400

    adds UICollectionView template class

---
+++ Librarius/LBRShelvesFlowCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  LBRShelvesFlowCollectionViewController.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 9/9/15.
+//  Copyright (c) 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBRShelvesFlowCollectionViewController : UICollectionViewController
+
+@end
