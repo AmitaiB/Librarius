8d5761d2eb0fdf839473767ad0bd9ee169a25c27
AmitaiB <blickstein@gmail.com>
2015-10-11 10:20:07 -0400

    adds borrowed decoration view from ICF, but not wired up yet

---
+++ Librarius/LBRBookshelfDecorationView.h
@@ -0,0 +1,15 @@
+//
+//  LBRBookshelfDecorationView.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 10/11/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBRBookshelfDecorationView : UICollectionReusableView
+
++ (NSString *)kind;
+
+@end
