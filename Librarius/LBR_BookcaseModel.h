32c39c95143bea70ef9e11e9bb5d931f41ee9ac9
AmitaiB <blickstein@gmail.com>
2015-10-16 12:31:13 -0400

    Adds bookcase model

---
+++ Librarius/LBR_BookcaseModel.h
@@ -0,0 +1,13 @@
+//
+//  LBR_BookcaseModel.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 10/16/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LBR_BookcaseModel : NSObject
+
+@end
---
+++ Librarius/LBR_BookcaseModel.m
@@ -0,0 +1,21 @@
+//
+//  LBR_BookcaseModel.m
+//  Librarius
+//
+//  Created by Amitai Blickstein on 10/16/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import "LBR_BookcaseModel.h"
+
+@interface LBR_BookcaseModel ()
+
+
+
+@end
+
+@implementation LBR_BookcaseModel
+
+
+
+@end
