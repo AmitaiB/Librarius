2dc42604d67dd8fa8c9f0904d8a50133485c4d95
AmitaiB <blickstein@gmail.com>
2015-10-16 09:59:31 -0400

    Adds new CollectionViewController to work with

---
+++ Librarius/LBR_BookcaseCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  LBR_BookcaseCollectionViewController.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 10/16/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBR_BookcaseCollectionViewController : UICollectionViewController
+
+@end
