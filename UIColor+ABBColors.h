427f9311ca2da995fbd955729a86d99c76051eff
AmitaiB <blickstein@gmail.com>
2015-10-18 23:35:54 -0400

    Adds Planted UIColor category

---
+++ UIColor+ABBColors.h
@@ -0,0 +1,21 @@
+//
+//  UIColor+ABBColors.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 10/18/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ABBColors)
+
++(UIColor *)irisBlueColor;
++(UIColor *)mySinColor;
++(UIColor *)asparagusColor;
++(UIColor *)deepCarrotOrangeColor;
++(UIColor *)crayonOrangeColor;
++(UIColor *)shirazColor;
++(UIColor *)armadilloColor;
+
+@end
---
+++ UIColor+ABBColors.m
@@ -0,0 +1,40 @@
+//
+//  UIColor+ABBColors.m
+//  Librarius
+//
+//  Created by Amitai Blickstein on 10/18/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import "UIColor+ABBColors.h"
+
+@implementation UIColor (ABBColors)
+
++(UIColor *)irisBlueColor {
+    return [UIColor colorWithRed:0 green:0.75 blue:0.75 alpha:1];
+}
+
++(UIColor *)mySinColor {
+    return [UIColor colorWithRed:0.97 green:0.7 blue:0.13 alpha:1];
+}
+
++(UIColor *)asparagusColor {
+    return [UIColor colorWithRed:0.52 green:0.69 blue:0.4 alpha:1];
+}
+
++(UIColor *)deepCarrotOrangeColor {
+    return [UIColor colorWithRed:0.95 green:0.42 blue:0.18 alpha:1];
+}
+
++(UIColor *)crayonOrangeColor {
+    return [UIColor colorWithRed:0.95 green:0.42 blue:0.13 alpha:1];
+}
+
++(UIColor *)shirazColor {
+    return [UIColor colorWithRed:0.64 green:0 blue:0.26 alpha:1];
+}
+
++(UIColor *)armadilloColor {
+    return [UIColor colorWithRed:0.29 green:0.29 blue:0.29 alpha:1];
+}
+@end
