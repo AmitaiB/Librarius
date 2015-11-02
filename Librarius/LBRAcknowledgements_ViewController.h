2cb0af4bc81dd45a43b609e2f0efb0a922d64714
AmitaiB <blickstein@gmail.com>
2015-11-02 15:46:52 -0500

    Adds licenses, but in random order

---
+++ Librarius/LBRAcknowledgements_ViewController.h
@@ -0,0 +1,14 @@
+//
+//  LBRAcknowledgements_ViewController.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 11/2/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBRAcknowledgements_ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextView *acknowledgmentsTextView;
+
+@end
---
+++ Librarius/NSString+ArrayWriter.h
@@ -0,0 +1,15 @@
+//
+//  NSString+ArrayWriter.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 11/2/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (ArrayWriter)
+
++ (NSString *)stringWithArray:(NSArray *)array;
+
+@end
