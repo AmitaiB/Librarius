c7c31172b3ef5ac318f95b6a2288887a398425ed
AmitaiB <blickstein@gmail.com>
2015-11-09 17:25:18 -0500

    Adds LibraryConstruction tableVC

---
+++ Librarius/LBR_LibraryConstruction_TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LBR_LibraryConstruction_TableViewController.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 11/9/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBR_LibraryConstruction_TableViewController : UITableViewController
+
+@end
