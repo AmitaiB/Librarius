1a07706a2598c180f9006e6087b115cd6ded2b16
AmitaiB <blickstein@gmail.com>
2015-11-14 18:47:07 -0500

    Adds coverFlowLayout, with NO impl - just a reminder

---
+++ Librarius/LBR_CoverFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  LBR_CoverFlowLayout.h
+//  Librarius
+//
+//  Created by Amitai Blickstein on 11/14/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBR_CoverFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ Librarius/LBR_CoverFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  LBR_CoverFlowLayout.m
+//  Librarius
+//
+//  Created by Amitai Blickstein on 11/14/15.
+//  Copyright © 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import "LBR_CoverFlowLayout.h"
+
+@implementation LBR_CoverFlowLayout
+
+@end
